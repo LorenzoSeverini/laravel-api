15d2fa17c7758c89136c7658d8b22384899866a1
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-10 16:15:32 +0200

    fix

--- resources/views/admin/projects/index.blade.php
+++ resources/views/admin/projects/index.blade.php
@@ -8,48 +8,31 @@
         <div class="col-md-12">
             <h1>Projects</h1>
             <a href="{{ route('admin.projects.create') }}" class="btn btn-primary my-3">Create</a>
-            <table class="table">
-                <thead>
-                    <tr>
-                        <th>ID</th>
-                        <th>Project Name</th>
-                        <th>Project Description</th>
-                        <th>Project Image</th>
-                        <th>Project Link</th>
-                        <th>Project Category</th>
-                        <th>Project Tags</th>
-                        <th>Actions</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    @foreach ($projects as $project)
-                    <tr>
-                        <td>{{ $project->id }}</td>
-                        <td>{{ $project->project_name }}</td>
-                        <td>{{ $project->project_description }}</td>
-                        <td>{{ $project->project_image }}</td>
-                        <td>{{ $project->project_link }}</td>
-                        <td>{{ $project->project_category }}</td>
-                        <td>{{ $project->project_tags }}</td>
-                        <td>
-                            <a href="{{ route('admin.projects.show', $project->id) }}" class="btn btn-primary">Show</a>
-                            <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-warning">Edit</a>
-                            <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST" class="d-inline">
-                                @csrf
-                                @method('DELETE')
-                                <button type="submit" class="btn btn-danger">Delete</button>
-                              </form>
-                        </td>
-                    </tr>
-                    @endforeach
-                </tbody>
-            </table>
-            {{-- paginate the projects --}}
-            {{ $projects->links() }}
         </div>
     </div>
+    <div class="row">
+        @foreach($projects as $project)
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">
+                        {{ $project->title }}
+                    </div>
+                    <div class="card-body">
+                        <img src="{{ asset('images/' . $project->image_path) }}" alt="image" width="100%">
+                        <p>{{ $project->description }}</p>
+                        <p>{{ $project->created_at }}</p>
+                        <a href="{{ route('admin.projects.edit', $project->id) }}" class="btn btn-primary">Edit</a>
+                        <form action="{{ route('admin.projects.destroy', $project->id) }}" method="POST">
+                            @csrf
+                            @method('DELETE')
+                            <button type="submit" class="btn btn-danger mt-2">Delete</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
 </div>
-
 @endsection
 
 
