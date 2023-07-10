23cd37b4622ce18b235a3e3c4f91d024787ccbff
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-10 17:00:27 +0200

    feat: fillable

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -8,4 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Project extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'excerpt',
+        'description',
+        'link',
+        'image',
+        'published_at',
+        'updated_at'
+    ];
 }
