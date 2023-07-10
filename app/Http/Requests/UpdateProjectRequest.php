3f03f8d6afddeea5639f4aefcefd077a3d2a19b1
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-10 17:00:18 +0200

    feat: function with data

--- app/Http/Requests/UpdateProjectRequest.php
+++ app/Http/Requests/UpdateProjectRequest.php
@@ -13,7 +13,7 @@ class UpdateProjectRequest extends FormRequest
      */
     public function authorize()
     {
-        return false;
+        return true;
     }
 
     /**
@@ -24,7 +24,30 @@ class UpdateProjectRequest extends FormRequest
     public function rules()
     {
         return [
-            //
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'url' => 'required|url',
+            'github' => 'required|url',
+            'image' => 'nullable|image',
+        ];
+    }
+    /**
+     * Get the validation messages that apply to the request.
+     *
+     * @return array<string, mixed>
+     */
+    public function messages()
+    {
+        return [
+            'title.required' => 'The title is required',
+            'title.min' => 'the title must be minimum 5 chart',
+            'title.max' => 'the title must be maximum 255 chart',
+            'description.required' => 'The description is required',
+            'url.required' => 'The url is required',
+            'url.url' => 'The url must be a valid url',
+            'github.required' => 'The github is required',
+            'github.url' => 'The github must be a valid url',
+            'image.image' => 'The image must be a valid image',
         ];
     }
 }
