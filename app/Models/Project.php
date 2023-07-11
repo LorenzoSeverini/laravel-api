050d7220253a0d56433a0f8ade5eacb3f861b6dc
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-11 16:38:44 +0200

    feat one to many realation

--- app/Models/Project.php
+++ app/Models/Project.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Type;
 
 class Project extends Model
 {
@@ -19,4 +20,10 @@ class Project extends Model
         'published_at',
         'updated_at'
     ];
+    // aggiungere nei model Type e Project i metodi per definire la relazione one to many
+
+    public function type()
+    {
+        return $this->belongsTo(Type::class);
+    }
 }
--- app/Models/type.php
+++ app/Models/type.php
@@ -4,8 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use App\Models\Project;
 
 class type extends Model
 {
     use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'icon'
+    ];
+
+    public function projects()
+    {
+        return $this->hasMany(Project::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'name';
+    }
 }
