322c3279adbdb585c0a8e37f5e989b6854446ac0
LorenzoSeverini <lorenzo.seve98@gmail.com>
2023-07-21 14:29:08 +0200

    fix

--- app/Http/Controllers/Api/ProjectController.php
+++ app/Http/Controllers/Api/ProjectController.php
@@ -20,4 +20,30 @@ class ProjectController extends Controller
 
         return response()->json($response);
     }
+
+    public function count()
+    {
+        $projects = Project::count();
+
+        $response = [
+            'success' => true,
+            'results' => $projects,
+            'message' => 'Projects found successfully'
+        ];
+
+        return response()->json($response);
+    }
+
+    public function show($id)
+    {
+        $project = Project::with("type", "technologies")->find($id);
+
+        $response = [
+            'success' => true,
+            'results' => $project,
+            'message' => 'Project found successfully'
+        ];
+
+        return response()->json($response);
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -19,4 +19,7 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::get("/projects/count", [ProjectController::class, 'count']);
 Route::get("/projects", [ProjectController::class, 'index']);
+Route::get("/projects/{id}", [ProjectController::class, 'show']);
+// Route::post("/projects", [ProjectController::class, 'store']);
